20e79ff787eef9bf05b58d47a8fc4d882f7511c5
Thaïs HERVOUËT gh api user/emails | jq -r '.[thais.prinborgne-hervouet@hotmail.fr].email <thais.prinborgne-hervouet@hotmail.fr>
2024-08-07 12:16:55 +0200

    scaffold restaurants

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_07_095353) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_07_101548) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
-    t.integer "rating"
+    t.string "phone_number"
+    t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
